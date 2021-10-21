59679ae20f9b672487394325176add94900ab616
dannysofftie <dankim761@gmail.com>
2021-10-21 07:03:10 +0300

    Add custom render, and fix failing build

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { waitFor } from '@testing-library/react';
 import App from './App';
+import { render } from './test-utils';
 
 describe('test <App />', () => {
-  test('should have expected text', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Write once/i);
-    expect(linkElement).toBeInTheDocument();
+  test('should have expected', async () => {
+    const { getByText } = render(<App />);
+    await waitFor(() => {
+      expect(getByText(/Write once/i)).toBeInTheDocument();
+    });
   });
 });

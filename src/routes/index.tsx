d471f22175d1042d0592b2bf368c4d266a0d7b21
dannysofftie <dankim761@gmail.com>
2021-10-21 06:51:02 +0300

    Enable code splitting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,33 @@
+import { chakra, Flex } from '@chakra-ui/react';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LandingPage from '../pages';
-import SignInPage from '../pages/sign-in';
-import SignUpPage from '../pages/sign-up';
+
+const LandingPage = lazy(() => import('../pages'));
+const SignInPage = lazy(() => import('../pages/sign-in'));
+const SignUpPage = lazy(() => import('../pages/sign-up'));
 
 const ApplicationRouter = () => {
   return (
     <Router basename="/">
-      <Switch>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route exact path="/sign-in">
-          <SignInPage />
-        </Route>
-        <Route exact path="/sign-up">
-          <SignUpPage />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <Flex justifyContent="center">
+            <chakra.img src={`/images/page-loading.gif`} />
+          </Flex>
+        }
+      >
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route exact path="/sign-in">
+            <SignInPage />
+          </Route>
+          <Route exact path="/sign-up">
+            <SignUpPage />
+          </Route>
+        </Switch>
+      </Suspense>
     </Router>
   );
 };
